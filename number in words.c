ab47df0da2acaa9b9047f50aa41aef230ba107fa
Pallavi Jain <jainpallavi717@gmail.com>
2026-02-10 19:49:05 +0530

    Add files via upload

---
+++ number in words.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+void one(int n){
+	switch(n){
+		case 1 : printf("one "); break;
+		case 2 : printf("two "); break;
+		case 3 : printf("three "); break;
+		case 4 : printf("four "); break;
+		case 5 : printf("five "); break;
+		case 6 : printf("six "); break;
+		case 7 : printf("seven "); break;
+		case 8 : printf("eight "); break;
+		case 9 : printf("nine "); break;
+	}
+}
+void tens(int n){
+	switch(n){
+		case 1 :  break;
+		case 2 : printf("twenty "); break;
+		case 3 : printf("thirty "); break;
+		case 4 : printf("forty "); break;
+		case 5 : printf("fifty "); break;
+		case 6 : printf("sixty "); break;
+		case 7 : printf("seventy "); break;
+		case 8 : printf("eighty "); break;
+		case 9 : printf("ninety "); break;
+	}
+}
+void teen(int n){
+    switch(n){
+        case 0: printf("ten "); break;
+        case 1: printf("eleven "); break;
+        case 2: printf("twelve "); break;
+        case 3: printf("thirteen "); break;
+        case 4: printf("fourteen "); break;
+        case 5: printf("fifteen "); break;
+        case 6: printf("sixteen "); break;
+        case 7: printf("seventeen "); break;
+        case 8: printf("eighteen "); break;
+        case 9: printf("nineteen "); break;
+    }
+}
+int main(){
+	int n;
+	printf("enter n : ");
+	scanf("%d",&n);
+	int th,h,t,o;
+	if(n<1000 || n>9999){
+		printf("please enter number of 4 digit");
+		return 0;
+	}
+	th= n/1000;
+	h = (n/100)%10;
+	t = (n/10)%10;
+	o = n%10;
+	one(th);
+	printf(" thousand ");
+	if(h!=0){
+		one(h);
+		printf(" hundred ");
+	}
+	if(t==1){
+		teen(o);;
+	}
+ else{
+        if(t != 0)
+            tens(t);
+        if(o != 0)
+            one(o);
+    }
+}
